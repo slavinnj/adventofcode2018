99372f51832a0459ce8e847bf29dd816de851638
Nick Slavin <nicholasjslavin@gmail.com>
2018-12-10 20:58:39 -0500

    Day 2 part 2

--- day2/aoc2.go
+++ day2/aoc2.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 	"io/ioutil"
 	"strings"
 )
@@ -14,16 +14,66 @@ func check(e error) {
 
 var file = "box_ids.txt"
 
+func difference(id1 string, id2 string) int {
+	diff := 0
+	for index, char := range id1 {
+		if id2[index] != byte(char) {
+			diff++
+		}
+	}
+	return diff
+}
+
 func main() {
 	data, err := ioutil.ReadFile(file)
 	check(err)
 	ids := strings.Split(string(data), "\n")
-	fmt.Printf("checksum = %d\n", chksum(ids))
+	chksum := chksum(ids)
+	fmt.Printf("checksum = %d\n", chksum)
+
+	match := false
+	index := 0
+	var box1 *string
+	var box2 *string
+	for !match {
+		id1 := ids[index]
+		for i := range ids {
+			diff := difference(id1, ids[i])
+			if diff == 1 {
+				box1 = &id1
+				box2 = &ids[i]
+				match = true
+				break
+			}
+		}
+		if !match && ((index + 1) < len(ids)) {
+			index++
+			id1 = ids[index]
+		}
+	}
+	if box1 != nil && box2 != nil {
+		fmt.Printf("Box 1 = %s\n", *box1)
+		fmt.Printf("Box 2 = %s\n", *box2)
+		box2_id := *box2
+		common := []string{}
+		common_str := ""
+		for pos, char := range *box1 {
+			if char == rune(box2_id[pos]) {
+				common = append(common, string(char))
+				common_str = strings.Join(common, "")
+			}
+		}
+		fmt.Printf("Common letters = %s\n", common_str)
+	} else {
+		fmt.Println("No matching boxes.")
+	}
 }
 
 func chksum(ids []string) int {
-	two_count := 0;
-	three_count := 0;
+	two_count := 0
+	three_count := 0
+	two_letter_boxes := make(map[string]struct{})
+	three_letter_boxes := make(map[string]struct{})
 	for i := 0; i < len(ids); i++ {
 		this_box := ids[i]
 		letter_frequencies := make(map[string]int)
@@ -41,9 +91,11 @@ func chksum(ids []string) int {
 			if v == 2 && seen_two == false {
 				two_count++
 				seen_two = true
+				two_letter_boxes[this_box] = struct{}{}
 			} else if v == 3 && seen_three == false {
 				three_count++
 				seen_three = true
+				three_letter_boxes[this_box] = struct{}{}
 			}
 		}
 	}
